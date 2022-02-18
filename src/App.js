e33273fee437b9e753f8de249f1827e4d4da3cbb
sakajamiu <sakaj20@gmail.com>
2022-02-18 19:37:03 +0100

    'v1'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {  useQuery } from '@apollo/client'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import PhoneForm from './components/PhoneForm'
+import { ALL_PERSONS } from './queries'
 
-function App() {
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+       <PersonForm setError={notify}/>
+       <PhoneForm/>
+   </div>
+  )
 }
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+    {errorMessage}
+    </div>
+  )
+}
+export default App
 
-export default App;
