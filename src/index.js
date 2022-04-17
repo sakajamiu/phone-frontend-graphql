127b70703e69888fa40bdcbd6c18fedfe40ae42c
sakajamiu <sakaj20@gmail.com>
2022-04-17 11:45:27 +0100

    'v2'

--- src/index.js
+++ src/index.js
@@ -20,23 +20,7 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: authLink.concat(httpLink)
 })
-const query = gql`
-query{
-  allPersons{
-    name,
-    phone,
-    address{
-      street,
-      city
-    }
-    id
-  }
-}
-`
-client.query({query})
-  .then((response) => {
-    console.log(response.data)
-  })
+
 ReactDOM.render(
   <ApolloProvider client ={client}>
     <App />
